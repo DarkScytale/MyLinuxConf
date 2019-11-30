2d09205bc2b678334740de39b9e23b1fea85c446
Sebastien VINOT <sebastien.vinot@prevention-informatique.fr>
2019-11-30 10:09:42 +0100

    Update setup.sh
    Clearing the setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,32 +1,28 @@
 /bin/sh
 
-echo "STEP 1: GETTING GIT FILES ..."
+echo "STEP 0: Preparing variables"
+root_folder="~/MyLinuxConf"
+root_folder_backup="~/MyLinuxConf.backup"
+
+echo "STEP 1: Preparing the environment"
+if [ -d ${root_folder} ]; then
+	rm -rf ${root_folder_backup}
+	mv ${root_folder} ${root_folder_backup}
+fi
+
+echo "STEP 1: Getting clean new files ..."
 echo "Cleaning Git deposit for my configuration and Gitting it."
-# git clone https://github.com/DarkScytale/MyLinuxConf.git
-rm -rf ~/MyLinuxConf
 cd ~
+git clone https://github.com/DarkScytale/MyLinuxConf.git
 
-echo "STEP 2: BACKING UP CURRENT CONFIGURATION ..."
-rm -rf ~/MyLinuxConf.backup
-mkdir ~/MyLinuxConf.backup
-mv ~/.zsh ~/MyLinuxConf.backup
-mv ~/.zshrc ~/MyLinuxConf.backup
-mv ~/.zsh_history ~/MyLinuxConf.backup
-mv ~/.tmux ~/MyLinuxConf.backup
-mv ~/.tmux.conf ~/MyLinuxConf.backup
-mv ~/.vim ~/MyLinuxConf.backup
-mv ~/.vimrc ~/MyLinuxConf.backup
+echo "STEP 2: Configuring ZSH ..."
+rm -f ~/.zshrc
+ln -s ${root_folder}/zsh/.zshrc ~/.zshrc
 
-echo "STEP 3: CONFIGURING ZSH ..."
-echo "Working with Zsh (assuming it has been installed) ..."
-mv ~/MyLinuxConf/zsh ~/.zsh
-ln -s ~/.zshrc ~/.zsh/.zshrc
+echo "STEP 3: Configuring tmux ..."
+rm -f ~/.tmux.conf
+ln -s ${root_folder}/tmux/.tmux.conf ~/.tmux.conf
 
-echo "STEP 4: CONFIGURING TMUX ..."
-mv ~/MyLinuxConf/tmux ~/.tmux
-ln -s ~/.tmux/.tmux.conf .
-
-echo "STEP 5: CONFIGURING VIM ..."
-echo "Not done"
-mv ~/MyLinuxConf/vim ~/.vim
-ln -s ~/.vim/.vimrc .
+echo "STEP 4: Configuring vim ..."
+rm -f ~/.vimrc
+ln -s ${root_folder}/vim/.vimrc ~/.vimrc
